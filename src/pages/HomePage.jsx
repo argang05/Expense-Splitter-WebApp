4be1723b0103eb9d994ed1bbef638746fc9989fb
Abhirup <argang05@gmail.com>
2025-01-12 19:26:53 +0530

    Minor change in UI

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -203,7 +203,7 @@ const HomePage = () => {
               <TripShortComponent key={trip.id} trip={trip} />
                 )) : 
                 <h1 className="text-2xl text-[#000249] md:text-4xl font-bold text-center">
-                No Trips To Show!
+                No Trips Found!
                 </h1>
               }
 
