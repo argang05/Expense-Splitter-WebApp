4e7a955103700ba6f9eca29997ac28339acf7f4b
Abhirup <argang05@gmail.com>
2025-01-13 14:54:35 +0530

    Minor adjustemnts

--- src/components/CreateBillForm.jsx
+++ src/components/CreateBillForm.jsx
@@ -309,9 +309,9 @@ const CreateBillForm = ({ tripGroupMembersIds, onClose, onBillFormSubmit, tripId
     formData.append("bill", JSON.stringify(billData));
     formData.append("image", image);
 
-    for (const [key, value] of formData.entries()) {
-      console.log(`${key}: ${value}`);
-    }
+    // for (const [key, value] of formData.entries()) {
+    //   console.log(`${key}: ${value}`);
+    // }
 
     onBillFormSubmit(formData); // Submit the form data
     onClose(); // Close the form
